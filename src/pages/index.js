f658ae182ecbf6e7b8fe07fdacf1bf18e23b4b1e
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2020-06-07 08:58:47 -0500

    Add context providers for services

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import {createMemoryHistory} from "history";
 import {Route, Router, Switch} from "react-router-dom";
 
+import FirebaseProvider from "components/FirebaseProvider/FirebaseProvider.jsx"
+import StripeProvider from 'components/StripeProvider/StripeProvider.jsx'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { SnackbarProvider } from "notistack";
+
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 import 'typeface-roboto';
 import 'typeface-roboto-slab';
@@ -11,19 +16,23 @@ import LandingPage from "./LandingPage/LandingPage.jsx";
 //import ProfilePage from "./ProfilePage/ProfilePage.jsx";
 //import LoginPage from "./LoginPage/LoginPage.jsx";
 
-import FirebaseProvider from "components/FirebaseProvider/FirebaseProvider.jsx"
-
-import '@stripe/stripe-js';
-import StripeProvider from "components/StripeProvider/StripeProvider.jsx"
-
 let hist = createMemoryHistory();
 
-export default () => (<FirebaseProvider>
-  <StripeProvider>
-    <Router history={hist}>
-      <Switch>
-        <Route path="/" component={LandingPage} />
-      </Switch>
-    </Router>
-  </StripeProvider>
-</FirebaseProvider>);
+export default () => {
+  return (<GoogleReCaptchaProvider
+    reCaptchaKey={process.env.RECAPTCHA_KEY}
+    language="es"
+  >
+    <SnackbarProvider>
+      <StripeProvider>
+        <FirebaseProvider>
+          <Router history={hist}>
+            <Switch>
+              <Route path="/" component={(LandingPage)} />
+            </Switch>
+          </Router>
+        </FirebaseProvider>
+      </StripeProvider>
+    </SnackbarProvider>
+  </GoogleReCaptchaProvider>)
+};
