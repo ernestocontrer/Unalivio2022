917929ed492612eb86f6bef293538e9676f6a58b
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2020-06-07 20:46:14 -0500

    Expose order notification api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,13 @@
-import {generate as _generateOrder, verify as _verifyOrder} from './orders'
+import {
+  generate as _generateOrder, 
+  verify as _verifyOrder,
+  notifyCreation as _notifyCreation,
+  notifyUpdate as _notifyUpdate
+} from './orders'
 
 
 //export const fetchPrice = _fetchPrice;
 export const generateOrder = _generateOrder;
 export const verifyOrder = _verifyOrder;
+export const notifyCreation = _notifyCreation;
+export const notifyUpdate = _notifyUpdate;
