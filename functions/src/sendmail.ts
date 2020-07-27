f9107829c5551de62b99ec1523669bda173a44dd
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2020-07-27 08:30:07 -0500

    Use sendgrid via nodemailer

--- functions/src/sendmail.ts
+++ functions/src/sendmail.ts
@@ -1,23 +1,26 @@
 import * as nodemailer from 'nodemailer';
+//import * as sgMail from '@sendgrid/mail';
 import * as functions from 'firebase-functions';
 
 
+const sgTransport = require('nodemailer-sendgrid-transport');
 
 // import nodemailer (after npm install nodemailer)
 
 
 // config for mailserver and mail
-const config = {
-  service: 'gmail',
+const options = {
+  //service: 'gmail',
   auth: {
-    user: functions.config().gmail.user,
-    pass: functions.config().gmail.pass // naturally, replace both with your real credentials or an application-specific password
+    api_key: functions.config().sendgrid.api_key
+    //user: functions.config().gmail.user,
+    //pass: functions.config().gmail.pass // naturally, replace both with your real credentials or an application-specific password
   }
 };
 
 const sendmail = async (mail: any) => {
   // create a nodemailer transporter using smtp
-  const transporter = nodemailer.createTransport(config);
+  const transporter = nodemailer.createTransport(sgTransport(options));
 
   // send mail using transporter
   try {
