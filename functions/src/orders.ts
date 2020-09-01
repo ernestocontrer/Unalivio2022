8781041a48a38d991855802b320831e111b81083
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2020-08-31 20:22:36 -0500

    Add local timestamp format to emails

--- functions/src/orders.ts
+++ functions/src/orders.ts
@@ -5,6 +5,10 @@ import {verifyApp} from './orders/verify.app';
 import {validate} from './orders/validate'
 import sendmail from './sendmail';
 
+const locale = 'es-MX';
+const options = {
+  timeZone: 'America/Mexico_City'
+}
 
 const currentRate = async (db: FirebaseFirestore.Firestore) => {
   const ratesQuery = await db.collection('rates').where('pair', '==', db.doc('pairs/VESMXN')).orderBy('time', 'desc').limit(1).get();
@@ -140,7 +144,7 @@ export const notifyCreation = (
           <li>Teléfono: ${order.to}</li>
           <li>Recarga: ${order.amount} Bs. S</li>
           <li>Cargo a tu tarjeta: ${order.price} MXN</li>
-          <li>Fecha y hora de recepción: ${order.created.toDate().toLocaleString()}</li>
+          <li>Fecha y hora de recepción: ${order.created.toDate().toLocaleString(locale, options)}</li>
         </ul>
         <br />
         <p>El siguiente paso es que tu orden se cobrará, y le haremos llegar la recarga a tu ser querido! Relájate y nosotros te mantenemos informados con dos correos más. 😌</p>
@@ -156,60 +160,63 @@ export const notifyUpdate = () => functions.firestore.document('orders/{orderId}
 
   if (!order) return;
 
-  if (order.settled) {
-    const mail = {
-      from: functions.config().gmail.user,
-      to: order.from,
-      bcc: functions.config().unalivio.bcc,
-      subject: `Has enviado UnAlivio a ${order.to} por ${order.amount} Bs. S. 🥳`,
-      html: `<div>
-        <h1>¡Tu Alivio ya fué Recargado!</h1>
-        <br />
-        <p>Confirmamos haber completado la recarga de tu orden con código #${context.params.orderId}:</p>
-        <ul>
-          <li>Teléfono: ${order.to}</li>
-          <li>Recarga: ${order.amount} Bs. S</li>
-          <li>Cargo a tu tarjeta: ${order.price} MXN</li>
-          <li>Fecha y hora de recepción: ${order.created.toDate().toLocaleString()}</li>
-          <li>Fecha y hora de recepción: ${order.succeeded.toDate().toLocaleString()}</li>
-          <li>Fecha y hora de recepción: ${order.settled.toDate().toLocaleString()}</li>
-        </ul>
-        <br />
-        <p>Tu ser querido ha sido aliviado, llámalo que ya tiene saldo! </p><br />
-        <p>Gracias y cuéntanos que opinas en Instagram <a target="_blank" href="https://instagram.com/esunalivio">@EsUnalivio</a>! </p>
-        <h6>Si algo salió mal por favor dínoslo a <a href="mailto:contacto@unalivio.com">contacto@unalivio.com</a> o respondiendo a éste correo, estamos para servirte.</h6>
-      </div>`
-    }
-
-    await sendmail(mail);
-    return;
-  }
 
   if (order.succeeded) {
-    const mail = {
-      from: functions.config().gmail.user,
-      to: order.from,
-      bcc: functions.config().unalivio.bcc,
-      subject: `UnAlivio para ${order.to} de ${order.amount} Bs. S.  ya fué cobrado 👏`,
-      html: `<div>
-        <h1>¡El cargo de UnAlivio ya fué realizado!</h1>
-        <br />
-        <p>Confirmamos el pago de tu orden con código #${context.params.orderId}:</p>
-        <ul style="font-size:80%">
-          <li>Teléfono: ${order.to}</li>
-          <li>Recarga: ${order.amount} Bs. S</li>
-          <li>Cargo a tu tarjeta: ${order.price} MXN</li>
-          <li>Fecha y hora de recepción: ${order.created.toDate().toLocaleString()}</li>
-          <li>Fecha y hora de cobro: ${order.succeeded.toDate().toLocaleString()}</li>
-        </ul>
-        <br />
-        <p>Muy pronto le llegará unalivio al celular de tu ser querido! ✨</p>
-        <h6>Si algo salió mal por favor dínoslo a <a href="mailto:contacto@unalivio.com">contacto@unalivio.com</a> o respondiendo a éste correo, estamos para servirte.</h6>
-      </div>`
+    if (order.settled && order.reference && order.provider) {
+      const mail = {
+        from: functions.config().gmail.user,
+        to: order.from,
+        bcc: functions.config().unalivio.bcc,
+        subject: `Has enviado UnAlivio a ${order.to} por ${order.amount} Bs. S. 🥳`,
+        html: `<div>
+          <h1>¡Tu Alivio ya fué Recargado!</h1>
+          <br />
+          <p>Confirmamos haber completado la recarga de tu orden con código #${context.params.orderId}:</p>
+          <ul>
+            <li>Teléfono: ${order.to}</li>
+            <li>Recarga: ${order.amount} Bs. S</li>
+            <li>Cargo a tu tarjeta: ${order.price} MXN</li>
+            <li>Fecha y hora de recepción: ${order.created.toDate().toLocaleString(locale, options)}</li>
+            <li>Fecha y hora de recepción: ${order.succeeded.toDate().toLocaleString(locale, options)}</li>
+            <li>Fecha y hora de recepción: ${order.settled.toDate().toLocaleString(locale, options)}</li>
+          </ul>
+          <br />
+          <p>Tu ser querido ha sido aliviado, llámalo que ya tiene saldo! </p><br />
+          <p>Gracias y cuéntanos que opinas en Instagram <a target="_blank" href="https://instagram.com/esunalivio">@EsUnalivio</a>! </p>
+          <h6>Si algo salió mal por favor dínoslo a <a href="mailto:contacto@unalivio.com">contacto@unalivio.com</a> o respondiendo a éste correo, estamos para servirte.</h6>
+        </div>`
+      }
+  
+      await sendmail(mail);
+      return;
+    } else if(!order.settled && !order.reference && !order.provider) {
+      const mail = {
+        from: functions.config().gmail.user,
+        to: order.from,
+        bcc: functions.config().unalivio.bcc,
+        subject: `UnAlivio para ${order.to} de ${order.amount} Bs. S.  ya fué cobrado 👏`,
+        html: `<div>
+          <h1>¡El cargo de UnAlivio ya fué realizado!</h1>
+          <br />
+          <p>Confirmamos el pago de tu orden con código #${context.params.orderId}:</p>
+          <ul style="font-size:80%">
+            <li>Teléfono: ${order.to}</li>
+            <li>Recarga: ${order.amount} Bs. S</li>
+            <li>Cargo a tu tarjeta: ${order.price} MXN</li>
+            <li>Fecha y hora de recepción: ${order.created.toDate().toLocaleString(locale, options)}</li>
+            <li>Fecha y hora de cobro: ${order.succeeded.toDate().toLocaleString(locale, options)}</li>
+          </ul>
+          <br />
+          <p>Muy pronto le llegará unalivio al celular de tu ser querido! ✨</p>
+          <h6>Si algo salió mal por favor dínoslo a <a href="mailto:contacto@unalivio.com">contacto@unalivio.com</a> o respondiendo a éste correo, estamos para servirte.</h6>
+        </div>`
+      }
+  
+      await sendmail(mail);
+      return;
     }
 
-    await sendmail(mail);
-    return;
   }
+
 });
 
