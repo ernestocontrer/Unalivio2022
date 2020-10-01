673e31f0f86bb9ad4c96e5fa24dfa3c143e8895a
Víctor G. G. Quiroga <master@localhost.localdomain>
2020-09-30 23:11:41 -0500

    Enable emulator suite

---
+++ src/services/emulator-suite.js
@@ -0,0 +1,23 @@
+const spawnDb = (firebase) => {
+  const db = firebase.firestore();
+  if (window.location.hostname === "localhost") {
+    db.settings({
+      host: "localhost:8080",
+      ssl: false
+    });
+  }
+  return db;
+}
+
+const spawnApi = (firebase) => {
+  const api = firebase.functions()
+  if (window.location.hostname === "localhost") {
+    api.useFunctionsEmulator(process.env.FIREBASE_FUNCTIONS_URL)
+  }
+  return api;
+}
+
+export {
+  spawnApi,
+  spawnDb
+}
