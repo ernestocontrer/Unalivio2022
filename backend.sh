205e5d0137d6c0bc387eb39f05225e1e9aabc7d7
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2020-12-16 23:25:56 -0600

    Save development firestore after use

--- backend.sh
+++ backend.sh
@@ -7,7 +7,7 @@ rm ./functions/.runtimeconfig.json
 cp ./functions/.runtimeconfig-development.json ./functions/.runtimeconfig.json
 nvm exec $(cat ./functions/.nvmrc) npm --prefix functions run build
 
-firebase emulators:start --import ./data/seed
+firebase emulators:start --import ./data/seed --export-on-exit ./data/seed --inspect-functions 
 
 rm ./functions/.runtimeconfig.json
 cp ./functions/.runtimeconfig-production.json ./functions/.runtimeconfig.json
