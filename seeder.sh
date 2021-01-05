fc4c8d91ec4c41ba968a4d9ac4d4eadcbedd2393
ＭＡＳＴＥＲキロガ <masterquiroga@protonmail.ch>
2021-01-05 08:04:33 -0600

    Fix firebase backend launchers

--- seeder.sh
+++ seeder.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
 
 
-NOW=$(date +%s)
+NOW=$(date +%Y-%m-%dT%x)
 echo "Seeding data..."
 TARGET_STORAGE_BUCKET="gs://aliviame-mvp.appspot.com/data/seeds/$NOW/firestore_export"
-SEED_PATH="./data/seed"
-METADATA_PATH=${SEED_PATH}/firebase-export-metadata.json
+SEED_PATH="./data/"
+#METADATA_PATH=${SEED_PATH}/firebase-export-metadata.json
 gcloud firestore export $TARGET_STORAGE_BUCKET #--collection-ids='amounts','currencies','methods','pairs','products','providers','rates'
-
 
 gsutil -m rsync -r $TARGET_STORAGE_BUCKET $SEED_PATH
 
-jq '.path = "data/seed" | .metadata_file = "firestore_export.overall_export_metadata"' ${METADATA_PATH} > ${METADATA_PATH}
+#jq ".path = \"${SEED_PATH}\" | .metadata_file = \"firestore_export.overall_export_metadata\"" ${METADATA_PATH} > ${METADATA_PATH}
 
 #firebase emulators:start --only firestore --import $SEED_PATH
