70ebea9e710ac9053822995edce492fcb86473d0
Rudolf Zaidel MacBook <rseidel@gmail.com>
2021-10-11 13:58:40 +0300

    a few comments was added

--- functions/src/generateUuid.ts
+++ functions/src/generateUuid.ts
@@ -1,11 +1,11 @@
-const { customAlphabet } = require('nanoid');
-
-let generateUuid = (ABC: string, length: number) => {
-	const alphabet = ABC;
-	const nanoid = customAlphabet(alphabet, length);
-	let uuid = nanoid().toString().split('');
-	uuid.splice(4, 0, '_0');
-	return (uuid = '0' + uuid.join(''));
-};
-
-export default generateUuid;
+// const { customAlphabet } = require('nanoid');
+//
+// let generateUuid = (ABC: string, length: number) => {
+// 	const alphabet = ABC;
+// 	const nanoid = customAlphabet(alphabet, length);
+// 	let uuid = nanoid().toString().split('');
+// 	uuid.splice(4, 0, '_0');
+// 	return (uuid = '0' + uuid.join(''));
+// };
+//
+// export default generateUuid;
