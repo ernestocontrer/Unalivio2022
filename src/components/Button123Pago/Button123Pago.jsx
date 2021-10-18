a338a345a219d087a615e9b6fef9de2b6597bc50
oleh-hrynchyshyn <oleh.hrynchyshyn@gmail.com>
2021-10-18 13:50:51 +0300

    add 123pago button to modal window

--- src/components/Button123Pago/Button123Pago.jsx
+++ src/components/Button123Pago/Button123Pago.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import Iframe from 'react-iframe-click';
-import productClasses from './ProductSection.module.css';
+import React from "react";
+import Iframe from "react-iframe-click";
+import productClasses from "./ProductSection.module.css";
 
 function Button123Pago(props) {
-	return (
-		<div
-			className={[productClasses.pagoButton, productClasses.bounce].join(' ')}
-		>
-			<Iframe
-				srcdoc={props.data}
-				width='210px'
-				height='110px'
-				scrolling='no'
-				className={productClasses.iframe}
-				style={{
-					border: 'none',
-				}}
-				onInferredClick={() => props.clearForms()}
-			></Iframe>
-		</div>
-	);
+  return (
+    <div
+      className={[productClasses.pagoButton, productClasses.bounce].join(" ")}
+    >
+      <Iframe
+        srcdoc={props.data}
+        width="210px"
+        height="110px"
+        scrolling="no"
+        className={productClasses.iframe}
+        style={{
+          /*      display: "none", */
+          border: "none",
+        }}
+        onInferredClick={() => {
+          console.log("zzzzzzzz");
+        }}
+      ></Iframe>
+    </div>
+  );
 }
 
 export default Button123Pago;
