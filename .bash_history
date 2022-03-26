65b4c8f9e29a48a02d0a3d626f167f39e47bd2c2
Rudolf Zaidel <my@ravin.com>
2022-03-26 13:02:51 +0200

    update with test_branch1 server changes

---
+++ .bash_history
@@ -0,0 +1,183 @@
+sudo apt update
+clear
+sudo apt install nginx
+git remote add origin https://gitlab.com/vnz1/venezuelamobile.git
+git init
+git remote add origin https://gitlab.com/vnz1/venezuelamobile.git
+git pull origin server
+cd
+cd vnz1
+ls
+cd server
+npm i
+sudo apt install npm
+curl http://http://localhost:8080/
+curl http://localhost:8080/
+curl http://localhost:8080
+node -v
+npm -v
+ls
+cd server
+npm i
+npm run start
+npm start
+cat app.yaml
+ls
+cd ..
+ls
+sudo mv default /server
+cd server
+ls
+cd ..
+ls
+cd etc
+cd /etc
+ls
+cd /nginx
+cd
+clear
+ls
+sudo mv default /etc/nginx/sites-available/default
+ls
+sudo nginx -t
+ls
+sudo mv default /etc/nginx/sites-available/default
+sudo nginx -t
+clear
+cd server
+node server.js
+cd
+sudo systemctl restart nginx
+cd server
+node server
+cd 
+npm i -g pm2
+cd server
+npm install -g pm2
+npm install  pm2
+pm2 start server.js
+ls
+cat package.json
+cd
+npm install pm2 -g
+sudo npm install pm2 -g
+cat package.json
+clear
+cd server
+pm2 start server.js
+cd
+clear
+pm2 startup systemd
+sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u devtestfornov --hp /home/devtestfornov
+pm2 status
+pm2 restart server
+git checkout test_branche
+git fetch
+git checkout test_branche
+git pull origin test_branche
+cd server
+car server.js
+cat server.js
+pm2 restart server
+cd server
+ping 10.128.0.3
+pm2 status
+cd server
+npm start
+pm2 
+curl http://localhost:8080
+ls
+git status
+git fetch
+cd server
+ls
+cat server.js
+pm2 status
+pm2 end
+pm2 command 
+pm2 help
+pm2 -h
+pm2 stop
+pm2 stop server
+git checkout test_branche1
+ls
+cat server
+cd
+ls
+cd server
+npm run start
+npm i
+cat server
+cat server.js
+pm2 start
+cd 
+ls
+clear
+sudo mv default /etc/nginx/sites-available/default
+sudo nginx -t\
+sudo nginx -t
+sudo systemctl restart nginx
+cd server
+npm start
+pm2 start
+npm i
+npm i -g pm2
+sudo npm i -g pm2
+pm2 start
+cd
+pm2 cd server
+clear
+cd server
+pm2 start server
+cd 
+cd server
+cat server
+cat server.js
+ls
+sudo mv server.js /server
+cd server
+ls
+car server.js
+cat server.js
+cd
+ls
+pm2 stop
+pm2 stop server
+cd server
+rm server.js
+ls
+cd 
+clear
+ls
+sudo mv server.js /server
+cd server
+ls
+cd
+ls
+mv server.js server
+cd server
+ls
+cat server.js
+pm2 start server
+pm2 restart servre
+pm2 restart server
+pm2 status
+pm2 stop server
+npm start
+npm i cors
+pm2 start server
+ды
+ls
+ps aux 
+ps aux | grep node
+cd /home/devtestfornov/
+ls 
+less server/server.js 
+ss -ltp
+ss -ltpn
+ping 10.128.0.1
+ping 10.128.0.3
+dmesg 
+sudo 0i
+sudo -i
+ss -ntlp
--- server/server.js
+++ server/server.js
@@ -16,81 +16,81 @@ const PAYALL_TRANSACTION_URL = `${VPN_URL}/payall/ws?wsdl`;
 
 
 const generateUuid = (ABC, length) => {
-    const alphabet = ABC;
-    const nanoid = customAlphabet(alphabet, length);
-    let uuid = nanoid().toString().split('');
-    uuid.splice(4, 0, '_0');
-    return (uuid = '0' + uuid.join(''));
+  const alphabet = ABC;
+  const nanoid = customAlphabet(alphabet, length);
+  let uuid = nanoid().toString().split('');
+  uuid.splice(4, 0, '_0');
+  return (uuid = '0' + uuid.join(''));
 };
 
 
 const createPayallTransaction = async (req, res) => {
-    const {data, time, id} = req.body.options;
+  const {data, time, id} = req.body.options;
 
-    const uuid = generateUuid("123456789", 17);
+  const uuid = generateUuid("123456789", 17);
 
-    const phoneNumber = data.to.replace(/-/g, "");
-    const args = {
-        arg0: {
-            uuid: uuid,
-            numero: phoneNumber,
-            monto: data.price,
-            operadora: data.product,
-            // producto: getProducto(data.product),
-            producto: req.body.producto,
-            pv: "4348",
-            pin: "81264062",
-            key: "HOcpMcgEDA4FEYX",
-            code: "####",
-        },
-    };
-    console.log(args);
-    try {
-        const client = await soap.createClient(PAYALL_TRANSACTION_URL);
+  const phoneNumber = data.to.replace(/-/g, "");
+  const args = {
+    arg0: {
+      uuid: uuid,
+      numero: phoneNumber,
+      monto: data.price,
+      operadora: data.product,
+      // producto: getProducto(data.product),
+      producto: req.body.producto,
+      pv: "4348",
+      pin: "81264062",
+      key: "HOcpMcgEDA4FEYX",
+      code: "####",
+    },
+  };
+  console.log(args);
+  try {
+    const client = await soap.createClient(PAYALL_TRANSACTION_URL);
 
-        const reacargarResponse = await client.recargar(args);
+    const reacargarResponse = await client.recargar(args);
 
-        const responseData = {
-            responseCode: reacargarResponse?.return?.codigo_respuesta,
-            message: reacargarResponse?.return?.string,
-            code: 'Payall'
-        }
-
-        if (reacargarResponse.return.codigo_respuesta === "00") {
-            return {
-                success: true,
-                ...responseData
-            }
-        } else {
-            return {
-                success: false,
-                ...responseData
-            }
-        }
-
-    } catch (e) {
-        console.error(e);
-        return {
-            success: false,
-        }
+    const responseData = {
+      responseCode: reacargarResponse?.return?.codigo_respuesta,
+      message: reacargarResponse?.return?.string,
+      code: 'Payall'
     }
+
+    if (reacargarResponse.return.codigo_respuesta === "00") {
+      return {
+        success: true,
+        ...responseData
+      }
+    } else {
+      return {
+        success: false,
+        ...responseData
+      }
+    }
+
+  } catch (e) {
+    console.error(e);
+    return {
+      success: false,
+    }
+  }
 }
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.post("/recargar", async (req, res) => {
-    const result = await createPayallTransaction(req, res);
+  const result = await createPayallTransaction(req, res);
 
-    res.send(result);
+  res.send(result);
 });
 
 
 const server = app.listen(8080, () => {
-    const host = server.address().address;
-    const port = server.address().port;
+  const host = server.address().address;
+  const port = server.address().port;
 
-    console.log(`server are listening on ${host} and ${port}`);
+  console.log(`server are listening on ${host} and ${port}`);
 });
