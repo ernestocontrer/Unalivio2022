c994df414009438770a8fde97cf34aaf9e6839ca
Rudolf Zaidel <my@ravin.com>
2022-03-29 11:44:08 +0300

    update payall balace on server and front

--- functions/src/balancePayall.ts
+++ functions/src/balancePayall.ts
@@ -1,44 +1,34 @@
 import * as functions from "firebase-functions";
-// import axios from "axios-https-proxy-fix";
+
+
+const middlewareServerUrl = 'http://34.66.120.175:8080/getBalance';
+
+async function postData(url: string, data: any) {
+    const result = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    });
+    return await result.json();
+}
+
 
 // todo Move to Node JS
 export const balancePayall = (db: FirebaseFirestore.Firestore) =>
-	functions.https.onCall((data) => {
-		// console.log(data);
-		// try {
-		// 	axios.get("34.66.120.175").then((res) => {
-		// 		console.log(res);
-		// 	});
-		// } catch (e) {
-		// 	console.log(e);
-		// }
-		// console.log(data);
-		// /*     const soap = require("soap-as-promised"); */
-		// const soap = require("soap");
-		// const url = "http://164.52.144.203:9967/payall/ws?wsdl";
-		// const args = {
-		// 	arg0: {
-		// 		pv: "4348", //idPV
-		// 		pin: "81264062", //pin
-		// 		key: "HOcpMcgEDA4FEYX", //IMEI22
-		// 		code: "####", //mac
-		// 	},
-		// };
-		// let amounts: Array<number> = [];
-		// (await db.collection("amounts").get()).docs.map((collection) => {
-		// 	collection.data().data.map((elem: any) => {
-		// 		amounts.push(elem.value);
-		// 	});
-		// });
-		// const maxAmount = amounts.sort((a, b) => b - a)[0];
-		// const payallPromise = new Promise((resolve, reject) => {
-		// 	soap.createClient(url, (err: any, client: any) => {
-		// 		client.saldo(args, (err: any, responce: any) => {
-		// 			console.log(responce.return.saldo_disponible);
-		// 			return resolve(!(maxAmount * 5 >= responce.return.saldo_disponible));
-		// 		});
-		// 	});
-		// });
-		// const res = await payallPromise;
-		// return false;
-	});
+    functions.https.onCall((data) => {
+        try {
+
+            let amounts: Array<number> = [];
+            (await db.collection("amounts").get()).docs.map((collection) => {
+                collection.data().data.map((elem: any) => {
+                    amounts.push(elem.value);
+                });
+            });
+            const maxAmount = amounts.sort((a, b) => b - a)[0];
+
+
+            return (await postData(middlewareServerUrl, {maxAmount}));
+
+        } catch (e) {
+            console.error(e);
+            return false
+        }
+    });
--- functions/src/soapApi.ts
+++ functions/src/soapApi.ts
@@ -11,22 +11,12 @@ import sendmail from "./sendmail";
 import {code} from "./statusCodes/payallCodeResponce";
 
 
-const middlewareServerUrl = 'http://34.66.120.175:8080';
+const middlewareServerUrl = 'http://34.66.120.175:8080/recargar';
 
 // create payall transaction
 async function postData(url: string, data: any) {
     const result = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        // mode: 'cors', // no-cors, *cors, same-origin
-        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: 'same-origin', // include, *same-origin, omit
-        // headers: {
-        //   'Content-Type': 'application/json'
-        //   'Content-Type': 'application/x-www-form-urlencoded',
-        // },
-        // redirect: 'follow', // manual, *follow, error
-        // referrerPolicy: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
     return await result.json();
 }
